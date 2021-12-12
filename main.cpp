52072317ea6441163e912750b3675e0a9fbb83cd
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-12 05:09:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int dim;
+    cin >> dim;
+    int elenco[dim];
+    int n;
+    for (n=0;n<dim;n++){
+        cin >> elenco[n];
+    };
+    cout << massimo(elenco,dim);
+    return 0;
 }
