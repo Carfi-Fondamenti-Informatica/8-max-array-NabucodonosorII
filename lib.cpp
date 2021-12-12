d7781416fda2f19dfc0f7a87651de5a57878d108
NabucodonosorII <91675628+NabucodonosorII@users.noreply.github.com>
2021-12-12 05:08:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,11 @@
-
+#include "lib.h"
+int massimo(int elenco[],int dim){
+    int max;
+    max =elenco[0];
+    for (int i=1; i< dim ; i++) {
+        if (elenco[i] > max) {
+            max = elenco[i];
+        };
+    }
+    return max;
+}
